efe7608e025e809c669477876472f5121a6b4ce0
fedemarchits <fedemarchits@gmail.com>
2022-05-21 16:47:11 +0200

    close #3, aggiunta funzionalità 3 + test

--- e-shop-manager/src/test/java/it/unipd/mtss/Business/OrderBillTest.java
+++ e-shop-manager/src/test/java/it/unipd/mtss/Business/OrderBillTest.java
@@ -59,11 +59,62 @@ public class OrderBillTest {
                 new EItem(ItemType.Processor, "Processor2", 9.40),
                 new EItem(ItemType.Processor, "Processor3", 34.00),
                 new EItem(ItemType.Processor, "Processor4", 22.00),
-                new EItem(ItemType.Processor, "Processor5", 28.90),
-                new EItem(ItemType.Mouse, "Mouse1", 35.30),
-                new EItem(ItemType.Keyboard, "Keyboard1", 46.20));
-        double sum = 13.00 + (9.40/2) + 34.00 + 22.00 + 28.90 + 35.30 + 46.20;
+                new EItem(ItemType.Processor, "Processor5", 28.90));
+        double sum = 13.00 + (9.40/2) + 34.00 + 22.00 + 28.90;
         double orderPrice = orderBill.getOrderPrice(list, user);
         assertEquals(sum, orderPrice, 0.001);
+    }
+
+    @Test
+    public void testMoreThanTenMouseOrder() throws BillException {
+        list = Arrays.asList(
+                new EItem(ItemType.Mouse, "Mouse1", 13.00),
+                new EItem(ItemType.Mouse, "Mouse2", 16.40),
+                new EItem(ItemType.Mouse, "Mouse3", 34.00),
+                new EItem(ItemType.Mouse, "Mouse4", 22.00),
+                new EItem(ItemType.Mouse, "Mouse5", 28.90),
+                new EItem(ItemType.Mouse, "Mouse6", 11.30),
+                new EItem(ItemType.Mouse, "Mouse7", 31.20),
+                new EItem(ItemType.Mouse, "Mouse8", 22.00),
+                new EItem(ItemType.Mouse, "Mouse9", 25.90),
+                new EItem(ItemType.Mouse, "Mouse10", 35.30));
+        double sum = 13.00 + 16.40 + 34.00 + 22.00 + 28.90 + (11.30 * 0) + 31.20 + 22.00 + 25.90 + 35.30;
+        double orderPrice = orderBill.getOrderPrice(list, user);
+        assertEquals(sum, orderPrice, 0.001);
+    }
+
+    @Test
+    public void testLessExpensiveItem(){
+        list = Arrays.asList(
+                new EItem(ItemType.Processor, "Processor1", 12.60),
+                new EItem(ItemType.Processor, "Processor2", 9.20),
+                new EItem(ItemType.Processor, "Processor3", 31.10),
+                new EItem(ItemType.Processor, "Processor4", 21.80),
+                new EItem(ItemType.Processor, "Processor5", 23.80));
+        double lessExp = orderBill.getPriceOfLessExpensiveItem(list, ItemType.Processor, 5);
+        assertEquals(9.20, lessExp, 0.001);
+    }
+
+    @Test
+    public void testLessExpensiveItemInListWithNotEnoughItem(){
+        list = Arrays.asList(
+                new EItem(ItemType.Processor, "Processor1", 12.60),
+                new EItem(ItemType.Processor, "Processor2", 9.20),
+                new EItem(ItemType.Processor, "Processor3", 31.10),
+                new EItem(ItemType.Processor, "Processor4", 21.80));
+        double lessExp = orderBill.getPriceOfLessExpensiveItem(list, ItemType.Processor, 5);
+        assertEquals(0, lessExp, 0.1);
+    }
+
+    @Test
+    public void testLessExpensiveItemWithDifferentType(){
+        list = Arrays.asList(
+                new EItem(ItemType.Processor, "Processor1", 12.60),
+                new EItem(ItemType.Processor, "Processor2", 9.20),
+                new EItem(ItemType.Processor, "Processor3", 31.10),
+                new EItem(ItemType.Processor, "Processor4", 21.80),
+                new EItem(ItemType.Processor, "Processor5", 23.80));
+        double lessExp = orderBill.getPriceOfLessExpensiveItem(list, ItemType.Mouse, 5);
+        assertEquals(0, lessExp, 0.1);
     }
 }
